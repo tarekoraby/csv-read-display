8196e2640b9b2a9bc1b68d765aea7af67b7fe0fc
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-03-07 11:46:13 +0200

    Upgrade Vaadin to 23.0.0

--- src/main/java/com/example/application/CSVView.java
+++ src/main/java/com/example/application/CSVView.java
@@ -10,16 +10,13 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.shared.util.SharedUtil;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
-@Route("csv")
-@RouteAlias("")
+@Route("")
 public class CSVView extends VerticalLayout {
     private Grid<String[]> grid = new Grid<>();
 
@@ -27,19 +24,19 @@ public class CSVView extends VerticalLayout {
         MemoryBuffer buffer = new MemoryBuffer();
         Upload upload = new Upload(buffer);
         upload.setAcceptedFileTypes(".csv");
-        upload.addSucceededListener(e -> displayCsv(buffer.getInputStream()));
-
+        upload.addSucceededListener(e -> {
+            displayCsv(buffer.getInputStream());
+        });
         add(upload, grid);
     }
 
     private void displayCsv(InputStream resourceAsStream) {
+        // Change the separator if needed to something else (for example, to ',').
         CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
         CSVReader reader =
                 new CSVReaderBuilder(new InputStreamReader(resourceAsStream)).withCSVParser(parser).build();
-
         try {
             List<String[]> entries = reader.readAll();
-
             String[] headers = entries.get(0);
             grid.removeAllColumns();
 
